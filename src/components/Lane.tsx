7eb9b8cb0ed47815259e58a180c4a6c5ff8e0cb1
Sven-Truschel <truschelsven@gmail.com>
2021-07-15 16:08:06 +0200

    Got some feedback about ts usage and implemented it

--- src/components/Lane.tsx
+++ src/components/Lane.tsx
@@ -1,42 +1,41 @@
-import { ComponentProps } from "react";
-
-import "./Ticket";
+import { TicketType } from "../types";
 import Ticket from "./Ticket";
 
 type Props = {
   name: string;
   laneId: number;
-  tickets?: ComponentProps<typeof Ticket>[] ;
+  tickets?: TicketType[];
+  addTicket: () => void;
 };
 
-const onDragOverHandler = (event: { preventDefault: () => void }) => {
-  event.preventDefault();
-  console.log("drag over");
+const Lane = ({ name, tickets, laneId, addTicket }: Props): JSX.Element => {
+  const onDragOverHandler = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    console.log("drag over");
+  };
+
+  const dropHandler = (event: any) => {
+    event.preventDefault();
+    console.log("drophandler");
+    console.log(event.target);
+    console.log(event);
+    addTicket();
+  };
+
+  return (
+    <div
+      className="lane"
+      onDragOver={onDragOverHandler}
+      onDrop={dropHandler}
+      data-lane-id={laneId}
+    >
+      <h4 className="title">{name}</h4>
+      {tickets &&
+        tickets.map((t) => {
+          return <Ticket name={t.name} description={t.description} />;
+        })}
+    </div>
+  );
 };
-
-const dropHandler = (event: any) => {
-  event.preventDefault();
-  console.log("drophandler");
-  console.log(event.target)
-  console.log(event)
-  // var data = event.dataTransfer.getData("ticket");
-  // event.target.appendChild(document.getElementById(data));
-};
-
-const Lane = ({ name, tickets, laneId }: Props): JSX.Element => (
-  <div className="lane" onDragOver={onDragOverHandler} onDrop={dropHandler} data-lane-id={laneId}>
-    <h4 className="title">{name}</h4>
-    {/* {tickets &&
-        tickets.map((t) => (
-
-          
-        ))} */}
-    {tickets &&
-      tickets.map((t) => {
-        console.log({'t': t.props})
-        return <Ticket name={t.props.name} description={t.props.description} />;
-      })}
-  </div>
-);
 
 export default Lane;
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export type TicketType = {
+  name: string;
+  description: string;
+  laneId: number;
+};
